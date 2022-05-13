80d2102d194b549fda148424cffc29a2b527845c
William Eustis <>
2022-05-13 14:39:27 -0500

    config

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ i_s = (64, 33)
 CONFIG = {
     "batch_size": 128,
     "num_workers": 4,
-    'hidden_dim': 256,
-    'depth': 8,
-    'heads': 32,
+    'hidden_dim': 192,
+    'depth': 6,
+    'heads': 26,
     "epochs": 200, 
     "wandb": True,
     "frac_tokens": 0.75,
@@ -13,7 +13,7 @@ CONFIG = {
     "patch_h": 4,
     "patch_w": 3,
     'weight_decay': .1,
-    'randaug_n-op': 3,
-    'randaug_mag': 11,
+    'randaug_n-op': 2,
+    'randaug_mag': 15,
     'dropout': .2
 } 
